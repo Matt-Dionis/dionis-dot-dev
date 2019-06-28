cec4b1bea64bcd5ca008318986a1eba2de0c662a
Matt Dionis <mattdionis@gmail.com>
2019-06-28 17:11:07 -0400

    More testing

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -16,14 +16,15 @@ export default class Test extends React.Component {
     )
       .then(x => x.json())
       .then(x => {
-        this.setState({ testString: x })
+        console.log('hi');
+        console.log(x);
       })
   }
 
   render() {
     return (
       <React.Fragment>
-        <h1>{this.state.testString}</h1>
+        <h1>Test</h1>
       </React.Fragment>
     )
   }
